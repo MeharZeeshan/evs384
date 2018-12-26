51399d8ce435fcabc2d6eb22855db4f20f39e8e2
DESKTOP-SEHO8T9\Muhammad Saqib Tariq <mrsaqibtariq@gmail.com>
2018-12-26 20:49:55 +0500

    git repo clone / registation instruction

---
+++ topics.php
@@ -0,0 +1,10 @@
+Add New Git Repo:
+
+=> git init
+=> git remote add origin https://github.com/Mteam777/evs384.git 
+// git clone  https://github.com/Mteam777/evs384.git
+
+=> git pull origin master -- download
+=> git add -A
+=> git commit -m "first laravel installation"
+=> git push origin master
